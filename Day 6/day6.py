8f75430933a3f98123673e45f0bb0b21fa272359
Vishaal Kalwani <vishaalk@umich.edu>
2018-12-25 01:02:06 -0800

    After day 6 part 2

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -27,7 +27,7 @@ def closest_coordinate(p1, coordinates):
     return minItem[0] if len(allMinItems) == 1 else '.'
 
 def closest_coordinate_p2(p1, coordinates):
-    totalDistance = sum([manhattan(p1, coord) for coord in coordinates)])
+    totalDistance = sum([manhattan(p1, coord) for coord in coordinates])
     return '#' if totalDistance < 10000 else '.'
 
 # calculate all the places in the grid 
@@ -71,13 +71,13 @@ def calculate_areas(grid, remaining_points):
     return areas
 
 # print(grid)
-areas = calculate_areas(grid, remaining_points)
+# areas = calculate_areas(grid, remaining_points)
 
 # we want to disqualify any points that are on the exterior of the grid, since those will go on forever
 # which means iterating through the outside of our grid
 # print(grid)
-print(areas)
-print(max(areas.items(), key=operator.itemgetter(1))[1])
+# print(areas)
+# print(max(areas.items(), key=operator.itemgetter(1))[1])
 
 def calculate_areas_p2(grid):
     size = 0
@@ -85,6 +85,8 @@ def calculate_areas_p2(grid):
         for j in range(yMax):
             if (grid[i][j] == '#'):
                 size += 1
-
     return size
 
+calculate_distances_p2(grid, coordinates)
+ans = calculate_areas_p2(grid)
+print(ans)
