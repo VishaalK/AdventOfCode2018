d34d4876629a8fd8293a8519e57884fb0d31d6ef
Vishaal Kalwani <vishaalk@umich.edu>
2019-01-08 23:12:52 -0800

    Add world_state tick

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -11,4 +11,17 @@ def string_to_grid(string):
     for line in loop.split('\n'):
         print(line)
 
+# in C++
+# grid of track types,
+# mutates and updates the grid
+# check for collision after each tick
+# return once finished
+
+# check for collision complexity
+# keep track of the cycle of each cart
+# keep track of the direction of each cart
+
 string_to_grid(loop)
+
+def tick(world_state):
+        return world_state
