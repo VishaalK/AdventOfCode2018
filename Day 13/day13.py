f4c234f7d15eb012a638716dd711e362c99d6c47
Vishaal Kalwani <vishaalk@umich.edu>
2019-01-10 23:33:45 -0800

    Got some super basic stuff working

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -33,14 +33,44 @@ def string_to_grid(string):
 # keep track of the direction of each cart
 
 
-string_to_grid(game_grid)
+# string_to_grid(game_grid)
 
 def create_game_grid(input_lines):
     grid_x = max(map(len, input_lines))
     grid_y = len(input_lines)
     return numpy.array([[' ']*grid_x for i in range(grid_y)])
 
+def populate_game_grid(lines):
+    return numpy.array([list(line) for line in lines])
+
+def get_points_from_grid(grid):
+    ret = []
+    # print(grid.shape)
+    rows, cols = grid.shape[0], grid.shape[1]
+    for i in range(rows):
+        for j in range(cols):
+            # print(grid[i,j])
+            if grid[i,j] in ['>', '<', 'v', '^']:
+                ret += [(i,j)]
+                # print((i,j))
+
+    return ret
+
+def compute_movements():
+    raise Exception("Sup")
+
+def update_grid():
+    raise Exception("Sup")
+
 def tick(world_state):
     return world_state
 
-print(create_game_grid(game_grid.split('\n')))
+# print(create_game_grid(game_grid.split('\n')))
+
+lines = tuple(map(lambda l: l.rstrip('\n'), open("gridtest.txt").readlines()))
+print('lines:', lines)
+grid = populate_game_grid(lines)
+print(grid)
+points = get_points_from_grid(grid)
+
+print(points)
