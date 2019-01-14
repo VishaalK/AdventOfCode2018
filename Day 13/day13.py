7209bcb9cf6cb78b278fba0faef404a1ee35b729
Vishaal Kalwani <vishaalk@umich.edu>
2019-01-13 21:14:05 -0800

    You know

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -1,4 +1,7 @@
 import numpy
+from collections import namedtuple
+
+Cart = namedtuple('Cart','position direction')
 
 loop = r"""
 /----\
@@ -16,8 +19,6 @@ game_grid = r"""
   \------/ 
 """
 
-# print(loop)
-
 def string_to_grid(string):
     for line in loop.split('\n'):
         print(line)
@@ -32,38 +33,10 @@ def string_to_grid(string):
 # keep track of the cycle of each cart
 # keep track of the direction of each cart
 
-
-# string_to_grid(game_grid)
-
-def create_game_grid(input_lines):
-    grid_x = max(map(len, input_lines))
-    grid_y = len(input_lines)
-    return numpy.array([[' ']*grid_x for i in range(grid_y)])
-
 def populate_game_grid(lines):
     return numpy.array([list(line) for line in lines])
 
-def get_points_from_grid(grid):
-    ret = []
-    # print(grid.shape)
-    rows, cols = grid.shape[0], grid.shape[1]
-    for i in range(rows):
-        for j in range(cols):
-            # print(grid[i,j])
-            if grid[i,j] in ['>', '<', 'v', '^']:
-                ret += [(i,j)]
-                # print((i,j))
-
-    return ret
-
-def compute_movements(points, grid):
-    first = points[0]
-    tile = grid[first[0], first[1]]
-    # we can assume the points go in order
-    raise Exception("Sup")
-
 def get_direction(tile):
-    north, south, east, west = (0, -1), (0, 1), (1, 0), (-1, 0)
     if tile == '>':
         return east
     elif tile == '<':
@@ -75,21 +48,96 @@ def get_direction(tile):
     else:
         raise Exception("getting direction for non-directional tile")
 
-# carts are position, directions
+def get_points_from_grid(grid):
+    ret = []
+    # print(grid.shape)
+    rows, cols = grid.shape[0], grid.shape[1]
+    for i in range(rows):
+        for j in range(cols):
+            # print(grid[i,j])
+            if grid[i,j] in ['>', '<', 'v', '^']:
+                ret += [Cart(position=(i,j), direction=get_direction(grid[i,j]))]
+                # print((i,j))
+
+    return ret
+
+def compute_movements(points, grid):
+    first = points[0]
+    tile = grid[first[0], first[1]]
+    # we can assume the points go in order
+    raise Exception("Sup")
+
+north, south, east, west = (0, -1), (0, 1), (1, 0), (-1, 0)
+
 def move_cart(cart):
-    cart.position += get_direction(cart.tile)
+    cart.position += cart.direction
     return cart.position
 
-def update_cart(cart, grid):
-    (new_x, new_y) = move_cart(cart)
-    cart.tile = grid[x, y]
+def get_new_cart_direction(current_direction, tile):
+    if tile == '\\':
+        if current_direction == east:
+            return south
+        elif current_direction == south:
+            return east
+        elif current_direction == north:
+            return west
+        elif current_direction == west:
+            return north
+        else:
+            raise Exception("Incorrect \\ direction")
+        # east -> south
+        # south -> east
+        # north -> west
+        # west -> north
+    elif tile == '/': 
+        if current_direction == east:
+            return north
+        elif current_direction == north:
+            return east
+        elif current_direction == west:
+            return south
+        elif current_direction == south:
+            return west
+        else:
+            raise Exception("Incorrect / direction")
+        # east -> north
+        # north -> east
+        # west -> south
+        # south -> west
+    else:
+        return current_direction
+
 # depending on where they end up, we change their direction
+assert get_new_cart_direction(north, '\\') == west
+assert get_new_cart_direction(west, '/') == south
+
+tile_to_direction = {
+    '>' : east,
+    '<' : west,
+    '^' : north,
+    'v' : south
+}
+
+direction_to_tile = {
+    east: '>',
+    west: '<',
+    north: '^',
+    south: 'v'
+}
 
 def update_grid():
     raise Exception("Sup")
 
 def tick(world_state):
     return world_state
+
+def print_grid(points, grid):
+    cp = numpy.copy(grid)
+    for p in points:
+        position = p.position
+        cp[position[0], position[1]] = direction_to_tile[p.direction]
+
+    return cp
 
 # returns pairs of points who collided, should technically only be one
 # can make it fast by doing binary search, but linear is probably enough
@@ -103,11 +151,13 @@ def collision(just_moved_point, points):
 assert collision((1,2), [(1,2), (3,4)]) == (0,(1,2))
 
 # print(create_game_grid(game_grid.split('\n')))
-
 lines = tuple(map(lambda l: l.rstrip('\n'), open("gridtest.txt").readlines()))
-print('lines:', lines)
-grid = populate_game_grid(lines)
-print(grid)
-points = get_points_from_grid(grid)
 
-print(points)
+# print('lines:', lines)
+grid = populate_game_grid(lines)
+print('grid:\n', grid)
+points = get_points_from_grid(grid)
+print('points:\n', points)
+
+pgrid = print_grid(points, grid)
+print('pgrid:\n', pgrid)
